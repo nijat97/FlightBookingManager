18f8997506ee23a2e05c36faaedd306f9227b1c4
nijat97 <nijathasanov97@gmail.com>
2021-12-03 19:31:49 +0100

    First commit
    Signed-off-by: nijat97 <nijathasanov97@gmail.com>

---
+++ FlightManager/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace FlightManager
+{
+    partial class Form1
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.flightNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameOfPerson = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Surname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PassportNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.flightAdd = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.flightAdd);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 507);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.flightNo,
+            this.nameOfPerson,
+            this.Surname,
+            this.PassportNo});
+            this.dataGridView1.Location = new System.Drawing.Point(250, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(663, 507);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // flightNo
+            // 
+            this.flightNo.HeaderText = "Flight Number";
+            this.flightNo.Name = "flightNo";
+            // 
+            // nameOfPerson
+            // 
+            this.nameOfPerson.HeaderText = "Name";
+            this.nameOfPerson.Name = "nameOfPerson";
+            // 
+            // Surname
+            // 
+            this.Surname.HeaderText = "Surname";
+            this.Surname.Name = "Surname";
+            // 
+            // PassportNo
+            // 
+            this.PassportNo.HeaderText = "Passport №";
+            this.PassportNo.Name = "PassportNo";
+            // 
+            // flightAdd
+            // 
+            this.flightAdd.Location = new System.Drawing.Point(14, 14);
+            this.flightAdd.Name = "flightAdd";
+            this.flightAdd.Size = new System.Drawing.Size(75, 23);
+            this.flightAdd.TabIndex = 0;
+            this.flightAdd.Text = "Add Flight";
+            this.flightAdd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 532);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn flightNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameOfPerson;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Surname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PassportNo;
+        private System.Windows.Forms.Button flightAdd;
+    }
+}
+
