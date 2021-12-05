4c437bb9e96fdefdb8712a899ed70d39258c4446
nijat97 <nijathasanov97@gmail.com>
2021-12-05 04:57:57 +0100

    Flights and Bookings forms are added
    Flights dataGrid is connected to the MSAccess table.
    
    Signed-off-by: nijat97 <nijathasanov97@gmail.com>

---
+++ FlightManager/Flights.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.OleDb;
+
+namespace FlightManager
+{
+    public partial class flights : Form
+    {
+        public flights()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = GetFlightsList();
+        }
+
+        private DataTable GetFlightsList()
+        {
+            DataTable dtFlights = new DataTable();
+
+            string connString = ConfigurationManager.ConnectionStrings["dbx"].ConnectionString;
+
+            using (OleDbConnection con = new OleDbConnection(connString))
+            {
+                using (OleDbCommand cmd = new OleDbCommand("SELECT * FROM Flights", con))
+                {
+                    con.Open();
+
+                    OleDbDataReader reader = cmd.ExecuteReader();
+
+                    dtFlights.Load(reader);
+                }
+            }
+
+                return dtFlights;
+        }
+    } 
+}
